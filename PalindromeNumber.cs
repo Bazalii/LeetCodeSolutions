5108d1c1205d0ef3ea3a9e2b42dec2c904405d4a
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-06 17:32:19 +0300

    feat: add Palindrome Number solution

---
+++ PalindromeNumber.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        string input = Convert.ToString(x);
+        for (int i = 0; i < input.Length / 2; i++)
+        {
+            if (input[i] != input[input.Length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

9a8608a2e56dc06235217ba20b3e6f1e2aa6af8f
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-07 23:19:07 +0300

    feat: add Single Number solution

---
+++ SingleNumber.cs
@@ -0,0 +1,20 @@
+﻿public class Solution 
+{
+    public int SingleNumber(int[] nums) 
+    {
+        if (nums.Length == 1)
+            return nums[0];
+        Dictionary<int, int> countDict = new Dictionary<int, int>();
+        foreach (int number in nums)
+        {
+            if (!countDict.ContainsKey(number))
+                countDict.Add(number, 1);
+            else
+            {
+                countDict.Remove(number);
+            }
+        }
+
+        return countDict.Keys.First();
+    }
+}

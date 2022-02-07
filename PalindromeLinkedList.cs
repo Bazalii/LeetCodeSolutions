68d1296a6e210c9642818a56de7a136a2f403246
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-07 18:46:17 +0300

    feat: add Palindrome Linked List solution

---
+++ PalindromeLinkedList.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public bool IsPalindrome(ListNode head) 
+    {
+        ListNode currentNode = head;
+        List<int> number = new List<int>();
+        while (currentNode.next != null)
+        {
+            number.Add(currentNode.val);
+            currentNode = currentNode.next;
+        }    
+        number.Add(currentNode.val);
+        int bottom = 0;
+        int top = number.Count - 1;
+        Console.WriteLine(top);
+        while(bottom < top)
+        {
+            if (number[bottom] != number[top])
+                return false;
+            bottom++;
+            top--;
+        }
+        
+        return true;
+    }
+}

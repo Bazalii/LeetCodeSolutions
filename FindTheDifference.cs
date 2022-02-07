31c07604ff68af8d3880eb2f2059ddcda8d9883c
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-07 14:39:38 +0300

    feat: add Find the Difference solution

---
+++ FindTheDifference.cs
@@ -0,0 +1,25 @@
+﻿public class Solution 
+{
+    public char FindTheDifference(string s, string t) 
+    {
+        string tCopy = t;
+        for (int i = 0; i < t.Length; i++)
+        {
+            int j;
+            for (j = 0; j < s.Length; j++)
+            {
+                if (t[i] == s[j])
+                {
+                    tCopy = tCopy.Substring(1);
+                    s = s.Substring(0, j) + s.Substring(j + 1);
+                    break;
+                }
+
+                if (j == s.Length - 1)
+                    return tCopy[0];
+            }
+        }
+
+        return tCopy[0];
+    }
+}

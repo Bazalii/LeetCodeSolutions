5dd3c0117fa974f1f02ceb657999a0db3d82bf3f
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-08 22:14:02 +0300

    feat: add Longest Common Prefix solution

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,37 @@
+﻿public class Solution 
+{
+    public string LongestCommonPrefix(string[] strs) 
+    {
+        if (strs.Length == 1)
+            return strs[0];
+        string answer = "";
+        for (int i = 0; i < strs.Length - 1; i++)
+        {
+            int length = strs[i].Length < strs[i + 1].Length ? strs[i].Length : strs[i + 1].Length;
+            int j;
+            for (j = 0; j < length; j++)
+            {
+                if (strs[i][j] != strs[i + 1][j])
+                {
+                    if (answer == "" || answer.Length > j)
+                    {
+                        answer = strs[i].Substring(0, j);
+                    }
+
+                    break;
+                }
+            }
+
+            if (j == length)
+            {
+                if (answer == "" || answer.Length > j)
+                {
+                    answer = strs[i].Substring(0, length);
+                }
+            }
+            if (answer == "")
+                return answer;
+        }
+        return answer;
+    }
+}

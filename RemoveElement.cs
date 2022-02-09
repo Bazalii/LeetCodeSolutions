ae1bd32723841fb3e480c4c38857fa44f7c6f45c
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-09 18:28:54 +0300

    feat: add Remove Element solution

---
+++ RemoveElement.cs
@@ -0,0 +1,18 @@
+﻿public class Solution 
+{
+    public int RemoveElement(int[] nums, int val) 
+    {
+        if (nums.Length == 0) return 0;
+        int iterator = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[iterator] = nums[i];
+                iterator++;
+            }
+        }
+        
+        return iterator;
+    }
+}

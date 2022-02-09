5b9a093c209e56dffaf7110f0bc60935499f07a6
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-09 13:56:52 +0300

    feat: add Valid Parentheses solution

---
+++ ValidParentheses.cs
@@ -0,0 +1,28 @@
+﻿public class Solution 
+{
+    public bool IsValid(string s) 
+    {
+        if (s.Length == 1)
+            return false;
+        var stack = new Stack<char>();
+        foreach (var elem in s)
+        {
+            if (elem == '(' || elem == '{' || elem == '[')
+                stack.Push(elem);
+            else
+            {
+                if (stack.Count == 0) 
+                    return false;
+                var value = stack.Pop();
+                if (elem == ')' && value != '(')
+                    return false;
+                if (elem == '}' && value != '{')
+                    return false;
+                if (elem == ']' && value != '[')
+                    return false;
+            }   
+        }
+        
+        return stack.Count == 0;
+    }
+}

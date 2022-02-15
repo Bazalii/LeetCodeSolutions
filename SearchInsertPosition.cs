1ecb844d5b6d7c0314226b66312fd085ec569712
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-15 22:06:10 +0300

    feat: add Search Insert Position solution

---
+++ SearchInsertPosition.cs
@@ -0,0 +1,24 @@
+﻿public class Solution 
+{
+    public int SearchInsert(int[] nums, int target) 
+    {
+        int bottom = 0;
+        int top = nums.Length - 1;
+        int mid = 0;
+        while (bottom <= top) 
+        {
+            mid = (bottom + top) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                top = mid -1;
+            else
+                bottom = mid + 1;
+        }
+        
+        if (nums[mid] < target)
+            return mid + 1;
+        
+        return mid;
+    }
+}

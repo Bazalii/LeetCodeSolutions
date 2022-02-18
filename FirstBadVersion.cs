8749e2d9bbc562142d3b3e3a6a19e655eee6682c
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-19 00:16:51 +0300

    feat: add First Bad Version solution

---
+++ FirstBadVersion.cs
@@ -0,0 +1,26 @@
+﻿/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl 
+{
+    public int FirstBadVersion(int n) 
+    {
+        int bottom = 0;
+        int top = n - 1;
+        int index = n;
+        while (bottom <= top)
+        {
+            long sum = (long)bottom + (long)top;
+            int mid = (int) (sum / 2);
+            if (IsBadVersion(mid))
+            {
+                top = mid - 1;
+                index = mid;
+            } 
+            else    
+                bottom = mid + 1;
+        }
+        
+        return index;
+    }
+}

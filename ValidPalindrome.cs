d99892fe880a4ba546160c45024bce36c19d876e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-19 16:30:29 +0300

    feat: add Valid Palindrome solution

---
+++ ValidPalindrome.cs
@@ -0,0 +1,24 @@
+﻿public class Solution 
+{
+    public bool IsPalindrome(string s) 
+    {
+        int left = 0;
+        int right = s.Length - 1;
+        while (left < right)
+        {
+            if (!Char.IsLetter(s[left]) && !Char.IsNumber(s[left]))
+                left++;
+            else if (!Char.IsLetter(s[right]) && !Char.IsNumber(s[right]))
+                right--;
+            else if (Char.ToLower(s[left]) != Char.ToLower(s[right]))
+                return false;
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        
+        return true;
+    }
+}

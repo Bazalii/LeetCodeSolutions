eabc71c7cc582d6f712f086905a8ed06a11988a1
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-20 12:10:55 +0300

    feat: add Length of Last Word solution

---
+++ LengthOfLastWord.cs
@@ -0,0 +1,16 @@
+﻿public class Solution 
+{
+    public int LengthOfLastWord(string s) 
+    {
+        int wasLetter = -1;
+        for (int i = s.Length - 1; i > -1; i--)
+        {
+            if (s[i] == ' ' && wasLetter != -1)
+                return wasLetter - i;
+            if (wasLetter == -1 && s[i] != ' ')
+                wasLetter = i;
+        }
+        
+        return wasLetter + 1;
+    }
+}

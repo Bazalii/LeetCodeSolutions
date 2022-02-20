cd6f1bb830a6419f74f647cc676d480cd66a8476
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-20 15:50:04 +0300

    feat: add Summary Ranges solution

---
+++ SummaryRanges.cs
@@ -0,0 +1,35 @@
+﻿public class Solution 
+{
+    public IList<string> SummaryRanges(int[] nums) 
+    {
+        if (nums.Length == 0)
+            return new List<string>(){};
+        List<string> result = new ();
+        int left = nums[0];
+        int current = left;
+        int right = left;
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (i < nums.Length && nums[i] == current + 1)
+            {
+                right = nums[i];
+                current = right;
+            }
+            else
+            {
+                if (right != left)
+                    result.Add($"{left}->{right}");
+                else
+                    result.Add($"{left}");
+                if (i < nums.Length)
+                {
+                    left = nums[i];
+                    right = left;
+                    current = left;
+                }
+            }
+        }
+        
+        return result;
+    }
+}

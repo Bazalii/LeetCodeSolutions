99752d09712ae147fd6595d7ad744b29c37ff107
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-20 11:42:31 +0300

    feat: add Move Zeroes solution

---
+++ MoveZeroes.cs
@@ -0,0 +1,18 @@
+﻿public class Solution 
+{
+    public void MoveZeroes(int[] nums) 
+    {
+        int notZeroes = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[notZeroes] = nums[i];
+                notZeros++;
+            }
+        }
+        
+        for (int i = notZeroes; i < nums.Length; i++)
+            nums[i] = 0;
+    }
+}

f110705478d6208baf3cf0fe4b124c12e1c253be
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-21 11:14:21 +0300

    feat: add Climbing Stairs solution

---
+++ ClimbStairs.cs
@@ -0,0 +1,16 @@
+﻿public class Solution 
+{
+    public int ClimbStairs(int n) 
+    {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int[] result = new int[n];
+        result[0] = 1;
+        result[1] = 2;
+        for (int i = 2; i < n; i++)
+            result[i] = result[i - 1] + result[i - 2];
+        return result[n - 1];
+    }
+}

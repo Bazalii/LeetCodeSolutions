769c810dff231404d2abba382329c89bf4d7fc89
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-21 11:22:12 +0300

    feat: add Fibonacci Number solution

---
+++ FibonacciNumber.cs
@@ -0,0 +1,14 @@
+﻿public class Solution 
+{
+    public int Fib(int n) 
+    {
+        int[] fib = new int[n + 1];
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++)
+            fib[i] = fib[i - 1] + fib[i - 2];
+        return fib[n];
+    }
+}

bf71c7d534d2900aba60da1fde0f4a424443ac06
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-22 23:35:02 +0300

    feat: add Happy Number solution

---
+++ HappyNumber.cs
@@ -0,0 +1,27 @@
+﻿public class Solution 
+{
+    public bool IsHappy(int n) 
+    {
+        if (n == 1)
+            return true;
+        int prev = n;
+        int iter = 0;
+        while (true)
+        {
+            int next = 0, current = prev;
+            while (current != 0)
+            {
+                int remainder = current % 10;
+                next += remainder * remainder;
+                current /= 10;
+            }
+            iter++;
+            if (next == 1)
+                return false;
+            else if (iter > 20)
+                return false;
+            else
+                prev = next;
+        }
+    }
+}

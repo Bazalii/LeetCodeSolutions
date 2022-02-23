230b146dd909758157f87bc2dad2d6566d36e3af
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-23 13:37:59 +0300

    feat: add Excel Sheet Column Number solution

---
+++ ExcelSheetColumnNumber.cs
@@ -0,0 +1,11 @@
+﻿public class Solution 
+{
+    public int TitleToNumber(string columnTitle) 
+    {
+        int result = 0;
+        int degree = 26;
+        for (int i = columnTitle.Length - 1; i > -1; i--)
+            result += (columnTitle[i] - 'A' + 1) * (int)Math.Pow(26, columnTitle.Length - i - 1);
+        return result;
+    }
+}

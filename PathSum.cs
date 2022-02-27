1005b73bf83750be6ce83361ff7b5330a32d1b3d
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-02-27 16:36:18 +0300

    feat: add Path Sum solution

---
+++ PathSum.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public bool HasPathSum(TreeNode root, int targetSum) 
+    {
+        if (root == null) return false;
+        return MakeSum(0, targetSum, root);
+    }
+    
+    public bool MakeSum(int sum, int target, TreeNode node)
+    {
+        bool leftSum = false;
+        bool rightSum = false;
+        sum += node.val;
+        if (node.left != null)
+        {
+            leftSum = MakeSum(sum, target, node.left);
+        }
+        if (node.right != null)
+        {
+            rightSum = MakeSum(sum, target, node.right);
+        }
+        return leftSum || rightSum || sum == target && node.left == null && node.right == null; 
+    }
+}

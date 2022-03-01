e06dc621402b802d16f1aefe56b9606a3581b4e9
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-01 11:42:31 +0300

    feat: add Pascal's Triangle solution

---
+++ Pascal'sTriangle.cs
@@ -0,0 +1,23 @@
+﻿public class Solution 
+{
+    public IList<IList<int>> Generate(int numRows) 
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+        result.Add(new List<int> {1});
+        if (numRows == 1) return result;
+        result.Add(new List<int>{1, 1});
+        for (int i = 1; i < numRows - 1; i++)
+        {
+            int[] row = new int [i + 2];
+            row[0] = 1;
+            row[i + 1] = 1;
+            for (int j = 1; j < i + 1; j++)
+            {
+                row[j] = result[i][j - 1] + result[i][j]; 
+            }
+            result.Add(row.ToList());
+        }
+        
+        return result;
+    }
+}

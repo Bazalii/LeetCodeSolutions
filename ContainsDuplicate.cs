5d12a8629697861a79fe3c1e770b3cc5e5f5d045
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-03 16:57:54 +0300

    feat: add Contains Duplicate solution

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,16 @@
+﻿public class Solution 
+{
+    public bool ContainsDuplicate(int[] nums) 
+    {
+        var dict = new Dictionary<int,int>();
+        foreach (int element in nums)
+        {
+            if (dict.ContainsKey(element))
+                return true;
+            else
+                dict.Add(element, 1);
+        }
+        
+        return false;
+    }
+}

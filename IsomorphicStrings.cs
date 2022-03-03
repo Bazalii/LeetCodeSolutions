a23d095a810a8f99a5453ec97f1bd00f949d805a
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-03 18:23:03 +0300

    feat: add Isomorphic Strings solution

---
+++ IsomorphicStrings.cs
@@ -0,0 +1,35 @@
+﻿public class Solution 
+{
+    public bool IsIsomorphic(string s, string t) 
+    {
+        var firstDict = new Dictionary<char, char>();
+        var secondDict = new Dictionary<char, char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (firstDict.ContainsKey(s[i]))
+            {
+                if (firstDict[s[i]] != t[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (secondDict.ContainsKey(t[i]))
+                {
+                    if (secondDict[t[i]] != s[i])
+                    {
+                        return false;
+                    }
+                }
+                else 
+                {
+                    firstDict.Add(s[i], t[i]);
+                    secondDict.Add(t[i], s[i]);
+                }
+            }
+        }
+        
+        return true;
+    }
+}

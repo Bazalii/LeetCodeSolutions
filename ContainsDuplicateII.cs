63f44e4091d8498fd8e3f31c42e7a77365af3ad2
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-03 17:01:15 +0300

    feat: add Contains Duplicate II solution

---
+++ ContainsDuplicateII.cs
@@ -0,0 +1,23 @@
+﻿public class Solution 
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k) 
+    {
+        var dict = new Dictionary<int, List<int>>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                foreach (int element in dict[nums[i]])
+                {
+                    if (i - element <= k)
+                        return true;
+                }
+                dict[nums[i]].Add(i);
+            }
+            else
+                dict.Add(nums[i], new List<int>(){i});
+        }
+        
+        return false;
+    }
+}

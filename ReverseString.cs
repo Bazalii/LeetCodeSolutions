75006814b0a5c7cc2f9c3e212e1ec69f33e419dc
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-11 22:31:33 +0300

    feta: add Reverse String solution

---
+++ ReverseString.cs
@@ -0,0 +1,13 @@
+﻿public class Solution 
+{
+    public void ReverseString(char[] s) 
+    {
+        var last = s.Length - 1;
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            var temp = s[i];
+            s[i] = s[last - i];
+            s[last - i] = temp;
+        }
+    }
+}

0af2b56f63ce9e1bcbbf8c9b39cce222daaa557b
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-12 22:13:28 +0300

    feat: add Power of Three solution

---
+++ PowerOfThree.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public bool IsPowerOfThree(int n)
+    {
+        if (n <= 0) return false;
+        while (n > 1)
+        {
+            if (n % 3 != 0)
+                return false;
+            n /= 3;
+        }
+
+        return true;
+    }
+}

1379bca72e903c2a5f1942a4d62af93ff42d4495
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-12 22:15:14 +0300

    feat: add Ransom Note solution

---
+++ RansomNote.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        if (ransomNote.Length > magazine.Length) return false;
+        var magStats = new Dictionary<int, int>();
+        foreach (var element in magazine)
+        {
+            if (!magStats.ContainsKey(element))
+                magStats.Add(element, 0);
+            magStats[element] += 1;
+        }
+
+        foreach (var element in ransomNote)
+        {
+            if (!magStats.ContainsKey(element) || magStats[element] == 0)
+                return false;
+            magStats[element] -= 1;
+        }
+
+        return true;
+    }
+}

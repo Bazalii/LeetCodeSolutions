4595728e1854c77014745cb7ded6697a0a9b4bab
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-13 00:08:28 +0300

    feat: add First Unique Character in a String solution

---
+++ FirstUniqueCharacterInAString.cs
@@ -0,0 +1,22 @@
+﻿public class Solution 
+{
+    public int FirstUniqChar(string s) 
+    {
+        var result = new Dictionary<char, int>();
+        foreach (var letter in s)
+        {
+            if (!result.ContainsKey(letter))
+                result.Add(letter, 1);
+            else
+                result[letter]++;
+        }
+            
+        foreach (var kvp in result)
+        {
+            if (kvp.Value == 1)
+                return s.IndexOf(kvp.Key);
+        }
+        
+        return -1;
+    }
+}

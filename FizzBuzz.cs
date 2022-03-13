bf52ca1737aacdfdb448aa0aec0c6806ea09bdeb
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-13 13:35:07 +0300

    feat: add Fizz Buzz solution

---
+++ FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        var result = new string[n];
+        var iterator = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            var current = i.ToString();
+            var firstCondition = i % 3 == 0;
+            var secondCondition = i % 5 == 0;
+            if (firstCondition && secondCondition)
+                current = "FizzBuzz";
+            else if (firstCondition)
+                current = "Fizz";
+            else if (secondCondition)
+                current = "Buzz";
+            result[iterator] = current;
+            iterator++;
+        }
+
+        return result;
+    }
+}

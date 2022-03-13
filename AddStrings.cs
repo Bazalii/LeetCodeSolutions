46f657a927f668c17f6fba90b85a2461677f8fcd
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-13 23:42:04 +0300

    feat: add Add Strings solution

---
+++ AddStrings.cs
@@ -0,0 +1,41 @@
+﻿public class Solution
+{
+    public string AddStrings(string num1, string num2)
+    {
+        var result = new List<char>();
+        string lesser;
+        string bigger;
+        if (num1.Length < num2.Length)
+        {
+            lesser = num1;
+            bigger = num2;
+        }
+        else
+        {
+            lesser = num2;
+            bigger = num1;
+        }
+
+        int current = 0;
+        for (int i = 1; i <= lesser.Length; i++)
+        {
+            var cur = bigger[^i] + lesser[^i] + current - '0' - '0';
+            result.Add(Convert.ToChar((cur % 10 + '0')));
+            current = cur / 10;
+        }
+
+        for (int i = lesser.Length + 1; i <= bigger.Length; i++)
+        {
+            var cur = bigger[^i] + current - '0';
+            result.Add(Convert.ToChar((cur % 10 + '0')));
+            current = cur / 10;
+        }
+
+        if (current != 0)
+            result.Add(Convert.ToChar(current + '0'));
+
+        var finalResult = result.ToArray();
+        Array.Reverse(finalResult);
+        return new string(finalResult);
+    }
+}

b0a26f1fc8c226c2b25df25148749949c10329d6
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-13 12:59:19 +0300

    feat: add Intersection of Two Arrays solution

---
+++ IntersectionOfTwoArrays.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+        var iterator1 = 0;
+        var iterator2 = 0;
+        var stats = new HashSet<int>();
+        var result = new List<int>();
+        while (iterator1 < nums1.Length && iterator2 < nums2.Length)
+        {
+            var firstItem = nums1[iterator1];
+            var secondItem = nums2[iterator2];
+            if (firstItem < secondItem)
+                iterator1++;
+            else if (firstItem > secondItem)
+                iterator2++;
+            else
+            {
+                stats.Add(firstItem);
+                iterator1++;
+                iterator2++;
+            }
+        }
+
+        return stats.ToArray();
+    }
+}

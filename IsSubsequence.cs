5620cf4a56178a99b58d4bd87eedfed414cbb078
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-15 16:44:09 +0300

    feat: add Is Subsequence solution

---
+++ IsSubsequence.cs
@@ -0,0 +1,18 @@
+﻿public class Solution 
+{
+    public bool IsSubsequence(string s, string t) 
+    {
+        if (s.Length == 0) return true;
+        if (t.Length == 0) return false;
+        var iterator = 0;
+        foreach (var letter in t)
+        {
+            if (letter == s[iterator])
+                iterator++;
+            if (iterator == s.Length)
+                return true;
+        }
+        
+        return iterator == s.Length;
+    }
+}

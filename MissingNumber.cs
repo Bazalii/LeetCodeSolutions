8e6bb9888e11441759342b01b278c04c63fe812e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-24 19:49:29 +0300

    feat: add Missing Number solution

---
+++ MissingNumber.cs
@@ -0,0 +1,15 @@
+﻿public class Solution 
+{
+    public int MissingNumber(int[] nums) 
+    {
+        var correctSum = 0;
+        var realSum = 0;
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            correctSum += i;
+            realSum += nums[i - 1];
+        }
+        
+        return correctSum - realSum;
+    }
+}

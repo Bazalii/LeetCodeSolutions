543eab69b05e2ee1afd4771e2d51030751ebaf89
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-03-28 23:19:28 +0300

    feat: add The K Weakest Rows in a Matrix solution

---
+++ TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    public int[] KWeakestRows(int[][] mat, int k)
+    {
+        var counter = 0;
+        var column = 0;
+        var result = new HashSet<int>();
+        while (counter < k && column < mat[0].Length)
+        {
+            for (int i = 0; i < mat.Length; i++)
+            {
+                if (!result.Contains(i) && mat[i][column] == 0)
+                {
+                    result.Add(i);
+                    counter++;
+                }
+
+                if (counter == k)
+                {
+                    break;
+                }
+            }
+
+            column++;
+        }
+
+        if (counter < k)
+        {
+            for (int i = 0; i < mat.Length; i++)
+            {
+                if (!result.Contains(i))
+                {
+                    result.Add(i);
+                    counter++;
+                }
+
+                if (counter == k)
+                {
+                    break;
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}

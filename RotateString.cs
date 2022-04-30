fb2fb8752f2fe9d04629765ad2409c9ad3ff4d32
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-04-30 14:18:43 +0300

    feat: add Rotate String solution

---
+++ RotateString.cs
@@ -0,0 +1,25 @@
+﻿public class Solution 
+{
+    public bool RotateString(string s, string goal) 
+    {
+        if (s.Length != goal.Length)
+        {
+            return false;
+        }
+        
+        var currentString = new StringBuilder(s, s.Length);
+        
+        for (int i = 0; i < s.Length; i++) 
+        {
+            if (currentString.ToString() == goal)
+            {
+                return true;
+            }
+            
+            currentString.Remove(0, 1);
+            currentString.Append(s[i]);
+        }
+        
+        return false;
+    }
+}

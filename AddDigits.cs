67a118731c3c55606e4cf7745d00b89efc1fbff8
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-05-10 14:29:41 +0300

    feat: add Add Digits solution

---
+++ AddDigits.cs
@@ -0,0 +1,19 @@
+﻿public class Solution 
+{
+    public int AddDigits(int num) 
+    {
+        while (num / 10 != 0) 
+        {
+            var result = 0;
+            while (num != 0) 
+            {
+                result += num % 10;
+                num /= 10;
+            }
+           
+            num = result;
+        } 
+        
+        return num; 
+    }
+}

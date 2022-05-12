ff39d631730b84cf6d529a0ce0129d7b3c68c5bd
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-05-12 18:09:22 +0300

    feat: add Shortest Distance to a Character solution

---
+++ ShortestDistanceToACharacter.cs
@@ -0,0 +1,39 @@
+﻿public class Solution 
+{
+    public int[] ShortestToChar(string s, char c) 
+    {
+        var wantedIndexes = new List<int>();
+        var result = new List<int>();
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == c)
+            {
+                wantedIndexes.Add(i);
+            }
+        }
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            var closestDistance = s.Length;
+            var currentDistance = s.Length;
+            var iterator = 0;
+
+            while (iterator < wantedIndexes.Count && currentDistance <= closestDistance)
+            {
+                closestDistance = currentDistance;
+                currentDistance = Math.Abs(i - wantedIndexes[iterator]);
+                iterator++; 
+            }
+            
+            if (currentDistance < closestDistance) 
+            {
+                closestDistance = currentDistance;
+            }
+            
+            result.Add(closestDistance);
+        }
+        
+        return result.ToArray();
+    }
+}

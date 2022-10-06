9d6610821fd65e7cc0d4e75300f3595baf24e3ce
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-06 22:42:38 +0300

    feat: add Power of Four solution

---
+++ PowerOfFour.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public bool IsPowerOfFour(int currentNumber)
+    {
+        while (currentNumber > 1)
+        {
+            if (currentNumber % 4 != 0)
+            {
+                return false;
+            }
+
+            currentNumber /= 4;
+        }
+
+        if (currentNumber == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}

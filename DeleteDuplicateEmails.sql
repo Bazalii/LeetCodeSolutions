cfe5c0669cc515ef9e3a7cb9b3ec78726b627ebc
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-04-29 20:00:44 +0300

    feat: add Delete Duplicate Emails solution

---
+++ DeleteDuplicateEmails.sql
@@ -0,0 +1,13 @@
+﻿#MySQL
+DELETE
+FROM Person
+WHERE Id NOT in
+    (
+        SELECT person.id
+        FROM
+        (
+            SELECT MIN(Id) AS id
+            FROM Person
+            GROUP BY Email
+        ) AS person
+    )

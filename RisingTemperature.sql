747ee4a532504435b31ce29f88b33f3400d762b8
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-04-30 20:32:04 +0300

    feat: add Rising Temperature solution

---
+++ RisingTemperature.sql
@@ -0,0 +1,6 @@
+﻿#MySQL
+SELECT DISTINCT firstWeather.id
+FROM Weather AS firstWeather,
+     Weather AS secondWeather
+WHERE datediff(firstWeather.recordDate, secondWeather.recordDate) = 1
+  AND firstWeather.temperature > secondWeather.temperature

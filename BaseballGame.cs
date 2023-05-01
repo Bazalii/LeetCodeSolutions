0670d49595c9ad7024f69f8eae61711adf684c98
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-01 22:26:18 +0300

    feat: add Baseball Game solution

---
+++ BaseballGame.cs
@@ -0,0 +1,28 @@
+﻿public class Solution 
+{
+    public int CalPoints(string[] operations) 
+    {
+        var results = new Stack<int>();
+
+        foreach (var operation in operations)
+        {
+            switch (operation) 
+            {
+                case "+":
+                    results.Push(results.ElementAt(0) + results.ElementAt(1));
+                    break;
+                case "D":
+                    results.Push(results.ElementAt(0) * 2);
+                    break;
+                case "C":
+                    results.Pop();
+                    break;
+                default:
+                    results.Push(int.Parse(operation));
+                    break;
+            }
+        }
+
+        return results.Sum();
+    }
+}

ad88b16592d7f8066318b6ec60bd4308671436ed
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-02 20:00:58 +0300

    feat: add Find All Numbers Disappeared in an Array solution

---
+++ FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,28 @@
+﻿public class Solution 
+{
+    public IList<int> FindDisappearedNumbers(int[] nums) 
+    {
+        var disappearedNumbers = new int[nums.Length];
+        var pointer = 0;
+
+        var numbers = nums.ToHashSet();
+
+        for (var i = 1; i <= nums.Length; i++) 
+        {
+            if (!numbers.Contains(i)) 
+            {
+                disappearedNumbers[pointer] = i;
+                pointer++;
+            }
+        }
+
+        var actualResult = new int[pointer];
+
+        for (int i = 0; i < pointer; i++) 
+        {
+            actualResult[i] = disappearedNumbers[i];
+        }
+
+        return actualResult;
+    }
+}

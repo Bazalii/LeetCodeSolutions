6a254029503a619105a0f92d51fd76bff62646fc
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-03 15:13:24 +0300

    feat: add Robot Return to Origin solution

---
+++ RobotReturnToOrigin.cs
@@ -0,0 +1,29 @@
+﻿public class Solution 
+{
+    public bool JudgeCircle(string moves) 
+    {
+        var x = 0;
+        var y = 0;
+
+        foreach (var move in moves) 
+        {
+            switch (move)
+            {
+                case 'R':
+                    x++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}

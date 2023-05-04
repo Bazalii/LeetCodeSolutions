f876bd19f46eade4eda80df6478c1a98f39b1354
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-04 16:42:10 +0300

    feat: add Hamming Distance solution

---
+++ HammingDistance.cs
@@ -0,0 +1,37 @@
+﻿public class Solution 
+{
+    public int HammingDistance(int x, int y) 
+    {
+        var distance = 0;
+
+        if (x < y) 
+        {
+            var temporaryValue = y;
+            y = x;
+            x = temporaryValue;
+        }
+
+        while (y != 0) 
+        {
+            if (x % 2 != y % 2) 
+            {
+                distance++;
+            }
+
+            x /= 2;
+            y /= 2;
+        }
+
+        while (x != 0) 
+        {
+            if (x % 2 != 0) 
+            {
+                distance++;
+            }
+
+            x /= 2;
+        }
+
+        return distance;
+    }
+}

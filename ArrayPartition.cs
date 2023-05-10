8f82d821436874cc409ee53ffd4ae45542b093d5
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-10 20:30:02 +0300

    feat: add Array Partition solution

---
+++ ArrayPartition.cs
@@ -0,0 +1,16 @@
+﻿public class Solution 
+{
+    public int ArrayPairSum(int[] nums) 
+    {
+        Array.Sort(nums);
+
+        var sum = 0;
+
+        for (var i = 0; i < nums.Length; i += 2)
+        {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+}

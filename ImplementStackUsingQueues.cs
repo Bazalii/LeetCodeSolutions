5638d0bcd3a6673949a15ca2730201fedc215ad9
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-11 15:01:30 +0300

    feat: add Implement Stack using Queues solution

---
+++ ImplementStackUsingQueues.cs
@@ -0,0 +1,45 @@
+﻿public class MyStack
+{
+    private readonly Queue<int> _queue = new();
+    private int _lastElement;
+
+    public void Push(int x)
+    {
+        _lastElement = x;
+
+        _queue.Enqueue(x);
+    }
+
+    public int Pop()
+    {
+        if (_queue.Count <= 1)
+        {
+            return _queue.Dequeue();
+        }
+        
+        var numberOfElementsToSave = _queue.Count - 1;
+
+        while (numberOfElementsToSave != 1)
+        {
+            _queue.Enqueue(_queue.Dequeue());
+
+            numberOfElementsToSave--;
+        }
+
+        _lastElement = _queue.Dequeue();
+
+        _queue.Enqueue(_lastElement);
+
+        return _queue.Dequeue();
+    }
+
+    public int Top()
+    {
+        return _lastElement;
+    }
+
+    public bool Empty()
+    {
+        return _queue.Count == 0;
+    }
+}

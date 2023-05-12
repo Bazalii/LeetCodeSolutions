66adcd6f8d87fdab2036625f5c9cfcbf730d43ac
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-12 13:31:12 +0300

    feat: add Middle of the Linked List solution

---
+++ MiddleOfTheLinkedList.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        var nodesCount = 0;
+        var currentNode = head;
+
+        while (currentNode is not null)
+        {
+            nodesCount++;
+
+            currentNode = currentNode.next;
+        }
+
+        var resultNode = head;
+
+        for (var i = 0; i < nodesCount / 2; i++)
+        {
+            resultNode = resultNode.next;
+        }
+
+        return resultNode;
+    }
+}

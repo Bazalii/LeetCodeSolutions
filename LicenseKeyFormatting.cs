4988da0a7111b45fb9e76dd972f85404501b3878
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-13 12:02:54 +0300

    feat: add License Key Formatting solution

---
+++ LicenseKeyFormatting.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public string LicenseKeyFormatting(string key, int sectionLength)
+    {
+        var formattedKey = new StringBuilder();
+
+        var currentSectionLength = 0;
+
+        for (var i = 1; i <= key.Length; i++)
+        {
+            if (key[^i] != '-')
+            {
+                if (currentSectionLength != sectionLength)
+                {
+                    formattedKey.Append(char.ToUpper(key[^i]));
+                    currentSectionLength++;
+                }
+                else
+                {
+                    formattedKey.Append('-');
+                    formattedKey.Append(char.ToUpper(key[^i]));
+                    currentSectionLength = 1;
+                }
+            }
+        }
+
+        return new string(formattedKey.ToString().Reverse().ToArray());
+    }
+}

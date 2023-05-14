56be5c9835e2c6bab154a327af34c2c271309967
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-14 11:24:20 +0300

    feat: add Distribute Candies solution

---
+++ DistributeCandies.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int DistributeCandies(int[] candyType)
+    {
+        var numberOfAllowedCandies = candyType.Length / 2;
+        var candyTypesHashSet = new HashSet<int>();
+
+        foreach (var type in candyType)
+        {
+            candyTypesHashSet.Add(type);
+        }
+
+        return candyTypesHashSet.Count < numberOfAllowedCandies ? candyTypesHashSet.Count : numberOfAllowedCandies;
+    }
+}

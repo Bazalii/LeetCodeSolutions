74eb2bdf3176aeb28b24605a92b556162d7d9ab3
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-15 12:29:34 +0300

    feat: add Maximum Depth of N-ary Tree solution

---
+++ MaximumDepthOfN-aryTree.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public int MaxDepth(Node root)
+    {
+        if (root is null)
+        {
+            return 0;
+        }
+
+        return CalculateHeight(root, 0);
+    }
+
+    private int CalculateHeight(Node node, int height)
+    {
+        height++;
+
+        if (node.children.Count == 0)
+        {
+            return height;
+        }
+
+        var biggestHeight = height;
+
+        for (var i = 0; i < node.children.Count; i++)
+        {
+            var currentHeight = CalculateHeight(node.children[i], height);
+
+            if (currentHeight > biggestHeight)
+            {
+                biggestHeight = currentHeight;
+            }
+        }
+
+        return biggestHeight;
+    }
+}

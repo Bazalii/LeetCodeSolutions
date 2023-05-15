019d5edb19840aa3eb7d7cb7241ac99af07105a0
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-15 12:49:34 +0300

    feat: add Employees Earning More Than Their Managers solution

---
+++ EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,6 @@
+﻿#MySQL
+SELECT employee.name AS Employee
+FROM Employee AS employee
+         JOIN Employee AS manager
+              ON employee.managerId = manager.id
+WHERE employee.salary > manager.salary

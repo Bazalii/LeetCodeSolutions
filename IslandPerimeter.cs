e3f0988df711cb871dc7df2c088e4673e2a6b1cc
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-17 20:05:24 +0300

    feat: add Island Perimeter solution

---
+++ IslandPerimeter.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+        var perimeter = 0;
+        var gridHeight = grid.Length;
+        var gridLength = grid[0].Length;
+
+        for (var y = 0; y < gridHeight; y++)
+        {
+            for (var x = 0; x < gridLength; x++)
+            {
+                if (grid[y][x] == 0)
+                {
+                    continue;
+                }
+
+                if (x - 1 == -1 || grid[y][x - 1] == 0)
+                {
+                    perimeter++;
+                }
+
+                if (x + 1 == gridLength || grid[y][x + 1] == 0)
+                {
+                    perimeter++;
+                }
+
+                if (y - 1 == -1 || grid[y - 1][x] == 0)
+                {
+                    perimeter++;
+                }
+
+                if (y + 1 == gridHeight || grid[y + 1][x] == 0)
+                {
+                    perimeter++;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}

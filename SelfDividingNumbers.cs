db413ab3311999c39dd8dfb7deb4693318ad5ad2
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-18 12:03:05 +0300

    feat: add Self Dividing Numbers solution

---
+++ SelfDividingNumbers.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public IList<int> SelfDividingNumbers(int left, int right)
+    {
+        var selfDividingNumbers = new List<int>();
+
+        while (left <= right)
+        {
+            var currentNumber = left;
+
+            while (currentNumber != 0)
+            {
+                var remainder = currentNumber % 10;
+
+                if (remainder == 0)
+                {
+                    break;
+                }
+
+                if (left % (remainder) != 0)
+                {
+                    break;
+                }
+
+                currentNumber /= 10;
+            }
+
+            if (currentNumber == 0)
+            {
+                selfDividingNumbers.Add(left);
+            }
+
+            left++;
+        }
+
+        return selfDividingNumbers;
+    }
+}

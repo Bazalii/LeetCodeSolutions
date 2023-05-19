3bd3ed0b5f36e2da451d3853c906c3c408d8add1
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-19 13:33:33 +0300

    feat: add Reverse Bits solution

---
+++ ReverseBits.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public uint reverseBits(uint n)
+    {
+        uint result = 0;
+        var factor = (uint) Math.Pow(2, 31);
+
+        while (n != 0)
+        {
+            var remainder = n % 2;
+
+            result += remainder * factor;
+
+            factor /= 2;
+            n /= 2;
+        }
+
+        return result;
+    }
+}

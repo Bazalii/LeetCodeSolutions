d3159f12ba1d5b90e1af5cee831408acc93984d4
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-20 18:14:12 +0300

    feat: add Assign Cookies solution

---
+++ AssignCookies.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int FindContentChildren(int[] children, int[] cookies)
+    {
+        Array.Sort(children);
+        Array.Sort(cookies);
+
+        var contentChildrenNumber = 0;
+
+        var currentChild = children.Length - 1;
+        var currentCookie = cookies.Length - 1;
+
+        while (currentChild != -1 && currentCookie != -1)
+        {
+            if (children[currentChild] <= cookies[currentCookie])
+            {
+                contentChildrenNumber++;
+
+                currentChild--;
+                currentCookie--;
+
+                continue;
+            }
+
+            currentChild--;
+        }
+
+        return contentChildrenNumber;
+    }
+}

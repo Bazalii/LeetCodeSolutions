350b1809366b39f06fd0e070082d32b31783d351
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-21 11:32:06 +0300

    feat: add Reverse Linked List solution

---
+++ ReverseLinkedList.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head is null)
+        {
+            return null;
+        }
+
+        var nodeValues = new List<int>();
+
+        while (head != null)
+        {
+            nodeValues.Add(head.val);
+
+            head = head.next;
+        }
+
+        head = new ListNode(nodeValues[^1]);
+        var currentNode = head;
+
+        for (var i = nodeValues.Count - 1; i > 0; i--)
+        {
+            var nextNode = new ListNode(nodeValues[i - 1]);
+            currentNode.next = nextNode;
+            currentNode = nextNode;
+        }
+
+        return head;
+    }
+}

0c93e534afb50b2024a3bd5c9abe2ddf2dfbc360
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-22 13:14:26 +0300

    feat: add Invert Binary Tree solution

---
+++ InvertBinaryTree.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public TreeNode InvertTree(TreeNode node)
+    {
+        if (node is null)
+        {
+            return null;
+        }
+
+        (node.left, node.right) = (node.right, node.left);
+
+        InvertTree(node.left);
+        InvertTree(node.right);
+
+        return node;
+    }
+}

3dd9ba968dcdb4e9014fbeffab9dccbe7f729777
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-23 13:20:30 +0300

    feat: add Number of Lines To Write String solution

---
+++ NumberOfLinesToWriteString.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int[] NumberOfLines(int[] widths, string word)
+    {
+        var numberOfLines = 1;
+        var lastLineLength = 0;
+
+        foreach (var letter in word)
+        {
+            var currentLetterWidth = widths[letter - 'a'];
+
+            if (lastLineLength + currentLetterWidth <= 100)
+            {
+                lastLineLength += currentLetterWidth;
+            }
+            else
+            {
+                numberOfLines++;
+                lastLineLength = currentLetterWidth;
+            }
+        }
+
+        return new[] { numberOfLines, lastLineLength };
+    }
+}

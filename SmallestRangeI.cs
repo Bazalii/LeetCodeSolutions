b92cb54ebfc1be0eaffd22c2510550d4415a369e
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-27 22:05:55 +0300

    feat: add Smallest Range I solution

---
+++ SmallestRangeI.cs
@@ -0,0 +1,9 @@
+﻿public class Solution 
+{
+    public int SmallestRangeI(int[] nums, int k)
+    {
+        var minimumDifference = nums.Max() - nums.Min() - 2 * k;
+
+        return minimumDifference < 0 ? 0 : minimumDifference;
+    }
+}

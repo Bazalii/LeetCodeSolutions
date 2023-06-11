0a68c1ed0990f724c98e4c90ba9462cba07811ff
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-11 12:11:32 +0300

    feat: realise Number Complement solution

---
+++ NumberComplement.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int FindComplement(int number)
+    {
+        var factor = 1;
+        var result = 0;
+
+        while (number != 0)
+        {
+            if ((number & 1) == 0)
+            {
+                result += factor;
+            }
+
+            number >>= 1;
+            factor <<= 1;
+        }
+        
+        return result;
+    }
+}

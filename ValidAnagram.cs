baddeb3d4065d5888b05df566c6d4098afd7c407
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-12 10:01:53 +0300

    feat: realise Valid Anagram solution

---
+++ ValidAnagram.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string initialString, string stringToCheck)
+    {
+        if (initialString.Length != stringToCheck.Length)
+        {
+            return false;
+        }
+
+        var letterCounters = new int [26];
+
+        for (var i = 0; i < initialString.Length; i++)
+        {
+            letterCounters[initialString[i] - 'a']++;
+            letterCounters[stringToCheck[i] - 'a']--;
+        }
+
+        return letterCounters.All(counter => counter == 0);
+    }
+}

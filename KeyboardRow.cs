0b1df389628993ef537b691e838e9ea2e2c861df
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-13 12:10:13 +0300

    feat: add Keyboard Row solution

---
+++ KeyboardRow.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    private const string FirstRow = "qwertyuiop";
+    private const string SecondRow = "asdfghjkl";
+    private const string ThirdRow = "zxcvbnm";
+
+    public string[] FindWords(string[] words)
+    {
+        var list = new List<string>();
+
+        foreach (var word in words)
+        {
+            var lowercaseWord = word.ToLower();
+            
+            var firstRowIntersection = lowercaseWord.Any(letter => FirstRow.Contains(letter));
+            var secondRowIntersection = lowercaseWord.Any(letter => SecondRow.Contains(letter));
+            var thirdRowIntersection = lowercaseWord.Any(letter => ThirdRow.Contains(letter));
+
+            if (firstRowIntersection && !secondRowIntersection && !thirdRowIntersection ||
+                !firstRowIntersection && secondRowIntersection && !thirdRowIntersection ||
+                !firstRowIntersection && !secondRowIntersection && thirdRowIntersection)
+            {
+                list.Add(word);
+            }
+        }
+
+        return list.ToArray();
+    }
+}

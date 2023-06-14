c302a3d640dca743d4cf12a3f224d76525168630
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-14 15:22:20 +0300

    feat: add Base 7 solution

---
+++ Base7.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string ConvertToBase7(int number)
+    {
+        var result = 0;
+        var degreeOfTen = 1;
+
+        while (number != 0)
+        {
+            result += number % 7 * degreeOfTen;
+            number /= 7;
+            degreeOfTen *= 10;
+        }
+
+        return result.ToString();
+    }
+}

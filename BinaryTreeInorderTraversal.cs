44aed3026bbc3c91be5f81b3799c0de565f6e662
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-15 10:37:12 +0300

    feat: update Binary Tree Inorder Traversal solution

--- BinaryTreeInorderTraversal.cs
+++ BinaryTreeInorderTraversal.cs
@@ -1,37 +1,31 @@
-﻿/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-public class Solution 
+﻿public class Solution
 {
-    public IList<int> InorderTraversal(TreeNode root) 
+    private readonly List<int> _nodes = new();
+
+    public IList<int> InorderTraversal(TreeNode root)
     {
-        if (root == null)
+        if (root is null)
+        {
             return new List<int>();
-        if (root.left == null && root.right == null)
-            return new List<int>(){ root.val };
-        var result = new List<int>();
-        var currentNode = root;
-        result = GoDown(root, result);
-        return result;
+        }
+
+        GoDown(root);
+
+        return _nodes;
     }
-    
-    public List<int> GoDown(TreeNode node, List<int> list)
+
+    private void GoDown(TreeNode node)
     {
-        if (node.left != null)
-            GoDown(node.left, list);
-        list.Add(node.val);
-        if (node.right != null)
-            GoDown(node.right, list);
-        return list;
+        if (node.left is not null)
+        {
+            GoDown(node.left);
+        }
+
+        _nodes.Add(node.val);
+
+        if (node.right is not null)
+        {
+            GoDown(node.right);
+        }
     }
 }

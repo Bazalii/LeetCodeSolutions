aea13dc60525442e12ae79d71815375d51177d06
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-23 17:22:47 +0300

    feat: add Validate Binary Search Tree solution

---
+++ ValidateBinarySearchTree.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsValidBST(TreeNode node)
+    {
+        return CheckSubtree(node, long.MinValue, long.MaxValue);
+    }
+
+    private bool CheckSubtree(TreeNode node, long leftBound, long rightBound)
+    {
+        if (node is null)
+        {
+            return true;
+        }
+
+        return node.val > leftBound && node.val < rightBound &&
+               CheckSubtree(node.left, leftBound, node.val) &&
+               CheckSubtree(node.right, node.val, rightBound);
+    }
+}

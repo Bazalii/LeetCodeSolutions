eac3523b786797cf214222f692a8295af0e25185
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-24 13:36:07 +0300

    feat: add Longest Substring Without Repeating Characters solution

---
+++ LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,31 @@
+﻿public class Solution 
+{
+    public int LengthOfLongestSubstring(string inputString) 
+    {
+        var substringCharacters = new HashSet<char>();
+        var longestSubstringLength = 0;
+        var rightPointer = 0;
+        var leftPointer = 0;
+
+        while (rightPointer < inputString.Length)
+        {
+            while (substringCharacters.Contains(inputString[rightPointer]))
+            {
+                substringCharacters.Remove(inputString[leftPointer]);
+                leftPointer++;
+            }
+
+            substringCharacters.Add(inputString[rightPointer]);
+
+            if (substringCharacters.Count > longestSubstringLength)
+            {
+                longestSubstringLength = substringCharacters.Count;
+            }
+
+            rightPointer++;
+        }
+        
+
+        return longestSubstringLength;
+    }
+}

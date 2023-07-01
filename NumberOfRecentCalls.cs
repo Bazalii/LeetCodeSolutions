fdb94b0b3785835c32920c9086ee6222a8a284af
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-07-01 19:54:42 +0300

    feat: add Number of Recent Calls solution

---
+++ NumberOfRecentCalls.cs
@@ -0,0 +1,17 @@
+﻿public class RecentCounter
+{
+    private readonly List<int> _calls = new();
+    private int _indexOfLatestCall = 0;
+
+    public int Ping(int time)
+    {
+        _calls.Add(time);
+
+        while (_calls[_indexOfLatestCall] < time - 3000)
+        {
+            _indexOfLatestCall++;
+        }
+
+        return _calls.Count - _indexOfLatestCall;
+    }
+}

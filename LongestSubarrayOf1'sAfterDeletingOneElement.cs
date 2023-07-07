9e2eb763b9060b31a90b7464d660aa886ae3e38e
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-07-07 23:10:28 +0300

    feat: add Longest Subarray of 1's After Deleting One Element solution

---
+++ LongestSubarrayOf1'sAfterDeletingOneElement.cs
@@ -0,0 +1,39 @@
+﻿public class Solution 
+{
+    public int LongestSubarray(int[] numbers) 
+    {
+        var numberOfZeroes = 0;
+        var rightPointer = 0;
+        var leftPointer = 0;
+        var longestSubarray = 0;
+
+        while (rightPointer < numbers.Length)
+        {
+            if (numbers[rightPointer] == 0) 
+            {
+                numberOfZeroes++;
+            }
+            
+            while (numberOfZeroes > 1)
+            {
+                if (numbers[leftPointer] == 0)
+                {
+                    numberOfZeroes--;
+                }
+
+                leftPointer++;
+            }
+            
+            var currentSubarrayLength = rightPointer - leftPointer;
+
+            if (currentSubarrayLength > longestSubarray)
+            {
+                longestSubarray = currentSubarrayLength;
+            }
+
+            rightPointer++;
+        }
+
+        return longestSubarray;
+    }
+}
